c4f951f7c70e9e02c77752e03118193322034965
khalidAtGit <khalid.work97@gmail.com>
2024-11-27 19:51:47 +0530

    images

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -5,7 +5,7 @@ import CategoryFilter from '../category-filter/category-filter.component';
 function Navbar() {
     return (
         <nav className='navbar'>
-            <img className='nav-logo' src="./assets/icon.png" alt="Logo" />
+            <img className='nav-logo' src="/assets/icon.png" alt="Logo" />
             <h1 className='nav-title'>MK ELECTRONICS</h1>
 
             <CategoryFilter />
